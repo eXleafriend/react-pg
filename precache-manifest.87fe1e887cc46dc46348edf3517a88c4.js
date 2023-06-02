ed73c06285ae17539ec1a1fdaf95cb3447c6b867
leafriend <leafriend@gmail.com>
2023-06-02 11:19:02 +0900

    Deploy React app to GitHub Pages

---
+++ precache-manifest.87fe1e887cc46dc46348edf3517a88c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "97c49e1c443ba591f4c5",
+    "url": "/react-pg/static/css/main.824b6060.chunk.css"
+  },
+  {
+    "revision": "97c49e1c443ba591f4c5",
+    "url": "/react-pg/static/js/main.97c49e1c.chunk.js"
+  },
+  {
+    "revision": "b3b486c717a974b0dc0f",
+    "url": "/react-pg/static/js/1.b3b486c7.chunk.js"
+  },
+  {
+    "revision": "313a75e49db1dde6bc2e",
+    "url": "/react-pg/static/css/2.6e1140dd.chunk.css"
+  },
+  {
+    "revision": "313a75e49db1dde6bc2e",
+    "url": "/react-pg/static/js/2.313a75e4.chunk.js"
+  },
+  {
+    "revision": "ff97c37021c77df59c47",
+    "url": "/react-pg/static/js/runtime~main.ff97c370.js"
+  },
+  {
+    "revision": "db9045d804ccd14dfd01c3c84b4a1666",
+    "url": "/react-pg/index.html"
+  }
+];
